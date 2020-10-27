49a1a4bdb7b2e245ca821288c2ec14f386ac8f8d
ClayData <claytonortiz96@gmail.com>
2020-10-27 15:08:49 -0500

    started servlets and html

---
+++ src/com/http/SignUpServlet.java
@@ -0,0 +1,16 @@
+package com.http;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SignUpServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		
+	}
+}
